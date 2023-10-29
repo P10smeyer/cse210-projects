cc008dd09f25c3644e9b788730b355c0851ba099
P10smeyer <preston.tensmeyer@gmail.com>
2023-10-28 20:21:47 -0600

    Adding comments to code.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -10,11 +10,13 @@ class Program
         int numberSquared = SquareNumber(number);
         DisplayResult(name, numberSquared);
 
+        // Displays Welcome Message.
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the Program!");
         }
 
+        // Prompt user for name and return a string name.
         static string PromptUserName()
         {
             Console.Write("Please enter your name: ");
@@ -22,6 +24,7 @@ class Program
             return name;
         }
 
+        // Prompt user for their favorite number and return a int number.
         static int PromptUserNumber()
         {
             Console.Write("What is your favorite number? ");
@@ -29,12 +32,14 @@ class Program
             return favoriteNumber;
         }
 
+        // Pass in number as a parameter and square the value. Return int number.
         static int SquareNumber(int number)
         {
             int numberSquared = number * number;
             return numberSquared;
         }
 
+        // Display the user name and favorite number.
         static void DisplayResult(string name, int squaredNumber)
         {
             Console.WriteLine($"{name}, The square of your favorite number is {squaredNumber}.");
