5741f6238c68fb7d553a6fa86fd3e5ac06901052
P10smeyer <preston.tensmeyer@gmail.com>
2023-10-30 21:54:05 -0600

    Initial Submit for Learning Activity 02. Contains 3 class which are used to generate a resume with job details.

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+//Class used for displaying job details. These job details can be used for a resume.
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+    
+}
