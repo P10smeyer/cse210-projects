1bc819cd17947774768a5782fe99c1ef46d0ced8
P10smeyer <preston.tensmeyer@gmail.com>
2023-11-24 22:12:51 -0700

    Initial commit for W05 Prepare: Learning Activity.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Blue", 2);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("Red", 5, 6);
+
+        Circle circle = new Circle("Green", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"The {color} shape has an area of of {area}.");
+        }
     }
 }
